f3aedd150e0b59aab73c6672dec7ee94088f01db
Tania A <ithiliel@gmail.com>
2019-03-02 19:50:22 +0100

    Work started on map. Custom tile class.

---
+++ fairybomb/Assets/Scripts/FairyBombTile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum TileType: int
+{
+    Block = 0,
+    Grass,
+    Wood
+}
+
+public class FairyBombTile : Tile
+{
+    [SerializeField] TileType _tile;
+    [SerializeField] bool _destructible;
+    [SerializeField] bool _walkable;
+
+
+#if UNITY_EDITOR
+    // The following is a helper that adds a menu item to create a RoadTile Asset
+    [MenuItem("Assets/Create/FAIRY BOMB/Create Terrain Tile")]
+    public static void CreateTerrainTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Terrain Tile", "New TerrainTile", "Asset", "Save Terrain Tile", "Assets");
+        if (path == "")
+            return;
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<FairyBombTile>(), path);
+    }
+#endif
+}
+
